45a69b22a6f7a60184b8b2cb565a5683dda0f9f2
vanessap94 <167862150+vanessap94@users.noreply.github.com>
2024-04-24 09:59:52 +0000

    Resolução exercício

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -1,11 +1,14 @@
 # Convenções para nomear as variáveis:
 # - Iniciar sempre com letra minúscula;
 # - O nome pode ser composto por letras, algarismos e underlines;
-# - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
+# - E não podemos usar palavras reservadas do Python, como if, fstr, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Paloma'
+string = nome
 
 # Declare variáveis de cada tipo numérico
-
-
+inter = 1
+floar =1.2
 # Declare uma variável do tipo booleano
+bool = True
--- exercicios/1-primeiros-passos/2-string.py
+++ exercicios/1-primeiros-passos/2-string.py
@@ -2,27 +2,38 @@ resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por is
 
 # Imprima na tela a variável "resumo"
 
+resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por isso está estudando 'python'."
 
 # Imprima na tela apenas a segunda letra da variável
+resumo [2]
 
 
 # Imprima na tela a idade de Paloma (resposta esperada: "46")
+idade = 46
 
 
 # Imprima na tela o trecho final da variável
+resumo [31:]
 
 
 # Converta todos as letras para minúsculo e imprima na tela
+resumo.lower ()
 
 
 # Converta todas as letras para maiúscula e imprima na tela
+resumo.upper ()
 
 
 # Formate a frase para que a primeira letra de cada palavra seja maiúscula e imprima na tela
+resumo.title ()
+
 
 
 # Formate a frase para que apenas a primeira letra da frase seja maiúscula e imprima na tela
+resumo.capitalize ()
 
 
 # Imprima na tela uma string utilizando uma variável, usando o recurso string format
+idade = 46
+print (f" Paloma é uma mulher de {idade} anos que deseja mudar de profissão, por isso está estudando 'python'.")
 

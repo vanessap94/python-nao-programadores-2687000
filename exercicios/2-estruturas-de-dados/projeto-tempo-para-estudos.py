3fb3af404f3d918a4908b47780b25b38899b9d0f
vanessap94 <167862150+vanessap94@users.noreply.github.com>
2024-04-24 13:53:13 +0000

    Resolução exercício

--- exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
+++ exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
@@ -1,6 +1,19 @@
 # Criaremos um script que imprimirá na tela o total de horas que uma pessoa estudou durante um determinado período:
+
 # 1. Crie uma variável chamada "nome" e, usando o método input(), atribua a ela um nome;
+nome = input ('Qual seu nome')
+              
 # 2. Crie uma variável chamada "total_dias" e, usando o método input(), solicite o total de dias dedicados ao estudo por semana;
+total_dias = input('Quantos dias por semana você costuma estudar?')
+
 # 3. Crie uma variável chamada "total_horas" e, usanod o método input(), solicite a média de horas estudada por dia;
+total_horas = input('Quantas horas por dia você costuma estudar?')
+
 # 4. Crie uma variável chamada "curso" e, usando o método input(), solicite o título do curso desejado;
+curso = input('Qual é o curso que você está fazendo agora?')
+
 # 5. Imprima na tela uma frase informando o nome da estudante, o total_dias dedicados aos estudos, o total horas semanais e o curso.
+total_dias = int(total_dias)
+total_horas = int(total_horas)
+
+print(f'Olá {nome}, você costuma estudar {total_dias} dias por semana, durante mais ou menos {total_horas} diárias. isso significa que você estuda em média {total_dias*total_horas} horas por semana para o curso {curso}.')
